debea8aee9d43f2df444f2c3f394b6332440e773
benmarwick <benmarwick@hotmail.com>
2012-03-20 11:37:18 -0700

    Update R/ggbiplot.r

--- R/ggbiplot.r
+++ R/ggbiplot.r
@@ -74,7 +74,7 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
     v <- pcobj$loadings
   } else if(class(pcobj) == 'PCA') {
     nobs.factor <- sqrt(nrow(pcobj$call$X))
-    d <- sqrt(pcobj$eig)
+    d <- unlist(sqrt(pcobj$eig)[1])
     u <- sweep(pcobj$ind$coord, 2, 1 / (d * nobs.factor), FUN = '*')
     v <- sweep(pcobj$var$coord,2,sqrt(pcobj$eig[1:ncol(pcobj$var$coord),1]),FUN="/")
   } else {
