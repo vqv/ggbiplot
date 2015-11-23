87cc31cf3cc888c5df960501713f808f7d8f2497
EhrmannS <steffen.ehrmann@biologie.uni-freiburg.de>
2015-11-23 15:34:06 +0200

    inclue additional graphics options (colour and alpha) for a set of defined columns.

--- R/ggbiplot.r
+++ R/ggbiplot.r
@@ -45,176 +45,215 @@
 #'   wine.pca <- prcomp(wine, scale. = TRUE)
 #'   print(ggbiplot(wine.pca, obs.scale = 1, var.scale = 1, groups = wine.class, ellipse = TRUE, circle = TRUE))
 #'
-ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE, 
-                      obs.scale = 1 - scale, var.scale = scale, 
-                      groups = NULL, ellipse = FALSE, ellipse.prob = 0.68, 
-                      labels = NULL, labels.size = 3, alpha = 1, 
-                      var.axes = TRUE, 
-                      circle = FALSE, circle.prob = 0.69, 
-                      varname.size = 3, varname.adjust = 1.5, 
-                      varname.abbrev = FALSE, ...)
+ggbiplot2 <- function (pcobj, choices = 1:2, coi = "all", scale = 1, pc.biplot = TRUE, # incl. new arg
+                       obs.scale = 1 - scale, var.scale = scale, groups = NULL, 
+                       ellipse = FALSE, ellipse.prob = 0.68, ellipse.alpha = 1, labels = NULL, labels.size = 3, 
+                       alpha = 1, var.axes = TRUE, circle = FALSE, circle.prob = 0.69, 
+                       varname.size = 3, varname.adjust = 1.5, varname.abbrev = FALSE, 
+                       arrow.color = muted("red"), arrow.linetype = "solid", arrow.alpha = 1, # incl. new args
+                       ...) 
 {
   library(ggplot2)
   library(plyr)
   library(scales)
   library(grid)
-
-  stopifnot(length(choices) == 2)
-
+  
   # Recover the SVD
- if(inherits(pcobj, 'prcomp')){
+  stopifnot(length(choices) == 2)
+  switch1 <- FALSE
+  if (inherits(pcobj, "prcomp")) {
     nobs.factor <- sqrt(nrow(pcobj$x) - 1)
     d <- pcobj$sdev
-    u <- sweep(pcobj$x, 2, 1 / (d * nobs.factor), FUN = '*')
+    u <- sweep(pcobj$x, 2, 1/(d * nobs.factor), FUN = "*")
     v <- pcobj$rotation
-  } else if(inherits(pcobj, 'princomp')) {
+  } else if (inherits(pcobj, "princomp")) {
+    # incl. switch to use later on for determination of package used for ordination.
+    switch1 <- TRUE
     nobs.factor <- sqrt(pcobj$n.obs)
     d <- pcobj$sdev
-    u <- sweep(pcobj$scores, 2, 1 / (d * nobs.factor), FUN = '*')
+    u <- sweep(pcobj$scores, 2, 1/(d * nobs.factor), FUN = "*")
     v <- pcobj$loadings
-  } else if(inherits(pcobj, 'PCA')) {
+    ln <- length(names(pcobj$scale))
+  } else if (inherits(pcobj, "PCA")) {
     nobs.factor <- sqrt(nrow(pcobj$call$X))
     d <- unlist(sqrt(pcobj$eig)[1])
-    u <- sweep(pcobj$ind$coord, 2, 1 / (d * nobs.factor), FUN = '*')
-    v <- sweep(pcobj$var$coord,2,sqrt(pcobj$eig[1:ncol(pcobj$var$coord),1]),FUN="/")
-  } else if(inherits(pcobj, "lda")) {
-      nobs.factor <- sqrt(pcobj$N)
-      d <- pcobj$svd
-      u <- predict(pcobj)$x/nobs.factor
-      v <- pcobj$scaling
-      d.total <- sum(d^2)
+    u <- sweep(pcobj$ind$coord, 2, 1/(d * nobs.factor), FUN = "*")
+    v <- sweep(pcobj$var$coord, 2, sqrt(pcobj$eig[1:ncol(pcobj$var$coord), 
+                                                  1]), FUN = "/")
+  } else if (inherits(pcobj, "lda")) {
+    nobs.factor <- sqrt(pcobj$N)
+    d <- pcobj$svd
+    u <- predict(pcobj)$x/nobs.factor
+    v <- pcobj$scaling
+    d.total <- sum(d^2)
   } else {
-    stop('Expected a object of class prcomp, princomp, PCA, or lda')
+    stop("Expected a object of class prcomp, princomp, PCA, or lda")
   }
-
+  
+  # manage columns of interest (coi)
+  if(switch1){
+    if(coi[1]=="all"){
+      coi = list(c(names(pcobj$scale)))
+      cnoi = NULL
+    } else {
+      if(!is.list(coi)){
+        stop("'coi' must be of type list")
+      }
+      cnoi <- names(pcobj$scale)[!names(pcobj$scale) %in% unlist(coi)]
+    }
+    lcoi <- length(coi)
+    lcnoi <- length(cnoi)
+  }
+  
   # Scores
   choices <- pmin(choices, ncol(u))
-  df.u <- as.data.frame(sweep(u[,choices], 2, d[choices]^obs.scale, FUN='*'))
-
+  df.u <- as.data.frame(sweep(u[, choices], 2, d[choices]^obs.scale, 
+                              FUN = "*"))
+  
   # Directions
-  v <- sweep(v, 2, d^var.scale, FUN='*')
+  v <- sweep(v, 2, d^var.scale, FUN = "*")
   df.v <- as.data.frame(v[, choices])
-
-  names(df.u) <- c('xvar', 'yvar')
+  
+  names(df.u) <- c("xvar", "yvar")
   names(df.v) <- names(df.u)
-
-  if(pc.biplot) {
+  if (pc.biplot) {
     df.u <- df.u * nobs.factor
   }
-
+  
   # Scale the radius of the correlation circle so that it corresponds to 
   # a data ellipse for the standardized PC scores
   r <- sqrt(qchisq(circle.prob, df = 2)) * prod(colMeans(df.u^2))^(1/4)
-
+  
   # Scale directions
   v.scale <- rowSums(v^2)
-  df.v <- r * df.v / sqrt(max(v.scale))
-
+  df.v <- r * df.v/sqrt(max(v.scale))
+  
   # Change the labels for the axes
-  if(obs.scale == 0) {
-    u.axis.labs <- paste('standardized PC', choices, sep='')
+  if (obs.scale == 0) {
+    u.axis.labs <- paste("standardized PC", choices, sep = "")
   } else {
-    u.axis.labs <- paste('PC', choices, sep='')
+    u.axis.labs <- paste("PC", choices, sep = "")
   }
-
+  
   # Append the proportion of explained variance to the axis labels
-  u.axis.labs <- paste(u.axis.labs, 
-                       sprintf('(%0.1f%% explained var.)', 
-                               100 * pcobj$sdev[choices]^2/sum(pcobj$sdev^2)))
-
+  u.axis.labs <- paste(u.axis.labs, sprintf("(%0.1f%% explained var.)", 
+                                            100 * pcobj$sdev[choices]^2/sum(pcobj$sdev^2)))
+  
   # Score Labels
-  if(!is.null(labels)) {
+  if (!is.null(labels)) {
     df.u$labels <- labels
   }
-
+  
   # Grouping variable
-  if(!is.null(groups)) {
+  if (!is.null(groups)) {
     df.u$groups <- groups
   }
-
+  
   # Variable Names
-  if(varname.abbrev) {
+  if (varname.abbrev) {
     df.v$varname <- abbreviate(rownames(v))
   } else {
     df.v$varname <- rownames(v)
   }
-
+  
   # Variables for text label placement
-  df.v$angle <- with(df.v, (180/pi) * atan(yvar / xvar))
-  df.v$hjust = with(df.v, (1 - varname.adjust * sign(xvar)) / 2)
-
+  df.v$angle <- with(df.v, (180/pi) * atan(yvar/xvar))
+  df.v$hjust = with(df.v, (1 - varname.adjust * sign(xvar))/2)
+  
+  # colour management
+  if(switch1){
+    alphas <- rep(1, ln)
+    colours <- rep(muted("red"), ln)
+    if(lcoi+1 != length(arrow.color) || lcoi+1!= length(arrow.linetype) || lcoi+1!= length(arrow.alpha)){
+      warning("styles can maybe not be changed meaningfully when the respective arguments don't have same lenghts.")
+    }
+    for(i in 1:(lcoi+1)){
+      if(i==1){
+        alphas[ names(pcobj$scale) %in% cnoi] <- arrow.alpha[i]
+        colours[ names(pcobj$scale) %in% cnoi] <- arrow.color[i]
+      } else {
+        alphas[ names(pcobj$scale) %in% coi[[i-1]] ] <- arrow.alpha[i]
+        colours[ names(pcobj$scale) %in% coi[[i-1]] ] <- arrow.color[i]
+      }
+    }
+  }
+  
   # Base plot
-  g <- ggplot(data = df.u, aes(x = xvar, y = yvar)) + 
-          xlab(u.axis.labs[1]) + ylab(u.axis.labs[2]) + coord_equal()
-
-  if(var.axes) {
+  g <- ggplot(data = df.u, aes(x = xvar, y = yvar)) + xlab(u.axis.labs[1]) + 
+    ylab(u.axis.labs[2]) + coord_equal()
+  
+  if (var.axes) {
     # Draw circle
-    if(circle) 
-    {
-      theta <- c(seq(-pi, pi, length = 50), seq(pi, -pi, length = 50))
-      circle <- data.frame(xvar = r * cos(theta), yvar = r * sin(theta))
-      g <- g + geom_path(data = circle, color = muted('white'), 
+    if (circle) {
+      theta <- c(seq(-pi, pi, length = 50), seq(pi, -pi, 
+                                                length = 50))
+      circle <- data.frame(xvar = r * cos(theta), yvar = r * 
+                             sin(theta))
+      g <- g + geom_path(data = circle, color = muted("white"), 
                          size = 1/2, alpha = 1/3)
     }
-
+    
     # Draw directions
-    g <- g +
-      geom_segment(data = df.v,
-                   aes(x = 0, y = 0, xend = xvar, yend = yvar),
-                   arrow = arrow(length = unit(1/2, 'picas')), 
-                   color = muted('red'))
+    g <- g + 
+      geom_segment(data = df.v, 
+                   aes(x = 0, y = 0, xend = xvar, yend = yvar), 
+                   arrow = arrow(length = unit(1/2, "picas")), 
+                   color = colours,
+                   linetype = arrow.linetype,
+                   alpha = alphas)
   }
-
+  
   # Draw either labels or points
-  if(!is.null(df.u$labels)) {
-    if(!is.null(df.u$groups)) {
+  if (!is.null(df.u$labels)) {
+    if (!is.null(df.u$groups)) {
       g <- g + geom_text(aes(label = labels, color = groups), 
                          size = labels.size)
     } else {
-      g <- g + geom_text(aes(label = labels), size = labels.size)      
+      g <- g + geom_text(aes(label = labels), size = labels.size)
     }
   } else {
-    if(!is.null(df.u$groups)) {
+    if (!is.null(df.u$groups)) {
       g <- g + geom_point(aes(color = groups), alpha = alpha)
     } else {
-      g <- g + geom_point(alpha = alpha)      
+      g <- g + geom_point(alpha = alpha)
     }
   }
-
+  
   # Overlay a concentration ellipse if there are groups
-  if(!is.null(df.u$groups) && ellipse) {
+  if (!is.null(df.u$groups) && ellipse) {
     theta <- c(seq(-pi, pi, length = 50), seq(pi, -pi, length = 50))
     circle <- cbind(cos(theta), sin(theta))
-
-    ell <- ddply(df.u, 'groups', function(x) {
-      if(nrow(x) <= 2) {
+    
+    ell <- ddply(df.u, "groups", function(x) {
+      if (nrow(x) <= 2) {
         return(NULL)
       }
       sigma <- var(cbind(x$xvar, x$yvar))
       mu <- c(mean(x$xvar), mean(x$yvar))
       ed <- sqrt(qchisq(ellipse.prob, df = 2))
-      data.frame(sweep(circle %*% chol(sigma) * ed, 2, mu, FUN = '+'), 
-                 groups = x$groups[1])
+      data.frame(sweep(circle %*% chol(sigma) * ed, 2, 
+                       mu, FUN = "+"), groups = x$groups[1])
     })
-    names(ell)[1:2] <- c('xvar', 'yvar')
-    g <- g + geom_path(data = ell, aes(color = groups, group = groups))
+    names(ell)[1:2] <- c("xvar", "yvar")
+    g <- g + geom_path(data = ell, aes(color = groups, group = groups), alpha = ellipse.alpha)
   }
-
+  
   # Label the variable axes
-  if(var.axes) {
+  if (var.axes) {
     g <- g + 
-    geom_text(data = df.v, 
-              aes(label = varname, x = xvar, y = yvar, 
-                  angle = angle, hjust = hjust), 
-              color = 'darkred', size = varname.size)
+      geom_text(data = df.v, 
+                aes(label = varname, x = xvar, y = yvar, angle = angle, hjust = hjust), 
+                color = colours, 
+                size = varname.size,
+                alpha = alphas)
   }
   # Change the name of the legend for groups
   # if(!is.null(groups)) {
   #   g <- g + scale_color_brewer(name = deparse(substitute(groups)), 
   #                               palette = 'Dark2')
   # }
-
+  
   # TODO: Add a second set of axes
-
+  
   return(g)
 }
